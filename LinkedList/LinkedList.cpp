26ed94eac345e9b9ec4c8c2bb2d9ee922bea22a5
Jin Han Jun <jinhanjun.contact@gmail.com>
2026-06-16 21:46:21 -0500

    added functions to insert elements into the linked list

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -14,6 +14,51 @@ void printList(Node* Nodeptr) {
 	}
 }
 
+void frontInsert(Node**head, int newValue) {
+	// first we create a newNode
+	Node* newNode = new Node();
+	newNode->Value = newValue;
+	// then point the newNode to the first element of the linked list
+	newNode->Next = *head;
+	// then we make the head of the linked list into the newNode we created
+	*head = newNode;
+
+}
+
+void endInsert(Node**head, int newValue) {
+	// first create a newNode
+	Node* newNode = new Node();
+	newNode->Value = newValue;
+	newNode->Next = NULL;
+	// if linked list is empty, newNode will be head node
+	if (*head == NULL) {
+		*head = newNode;
+		return;
+	}
+	// find last node
+	Node* last = *head;
+	while (last->Next != NULL) {
+		last = last->Next;
+	}
+	// insert newNode after last node
+	last->Next = newNode;
+}
+
+void afterInsert(Node* previous, int newValue) {
+	// check if previous node is NULL
+	if (previous == NULL) {
+		cout << "Previous can not be NULL";
+		return;
+	}
+	// create newNode
+	Node* newNode = new Node();
+	newNode->Value = newValue;
+	// insert newNode after previous
+	newNode->Next = previous->Next;
+	previous->Next = newNode;
+
+}
+
 int main() {
 	
 	Node* head = new Node();
@@ -29,6 +74,8 @@ int main() {
 	third->Value = 3;
 	third->Next = NULL;
 
+	afterInsert(head, -1);
+	afterInsert(second, -2);
 	printList(head);
 
 	system("pause>0");
